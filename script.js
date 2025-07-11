0c7f6e2384d5cacdc8e84e47f71eb84d9c302154
FreecityDong <1749697705@qq.com>
2025-07-11 10:54:39 +0800

    v1.3

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 
 function toggleMenu() {
   const menu = document.querySelector('nav.menubar');
-  if (menu) menu.classList.toggle('show');
+  const body = document.body;
+
+  if (menu.classList.contains('show')) {
+    menu.classList.remove('show');
+    body.classList.remove('no-scroll'); // ✅ 恢复滚动
+  } else {
+    menu.classList.add('show');
+    body.classList.add('no-scroll'); // ✅ 禁止滚动
+  }
 }
+
 
 function toggleSidebar() {
   const sidebar = document.querySelector('aside');
